fdea95e030434d4bc5e6d8f92fc33112c2a5e32d
sergeyteleshev <sergeyteleshev@yandex.ru>
2018-11-29 15:46:50 +0300

    seeder books

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function run()
+    {
+        foreach (range(1, 100) as $index)
+        {
+            DB::table('books')->insert([
+                'name' => str_random(10),
+                'desc' => str_random(500),
+                'author' => str_random(20),
+                'rating' => random_int(0,5),
+                'slogan' => str_random(100),
+                'sloganENG' => str_random(100),
+                'subject' => random_int(1,5),
+                'amountOfDownloads' => 0,
+                'imgURL' => "https://images.ua.prom.st/941882038_w640_h640_elrod_h.magiya__200na140mm.jpg",
+                'pagesBook' => (string) random_int(500,1000),
+                'pagesAbstarct' => (string) random_int(500,1000),
+                'publisher' => str_random(100),
+                'linkOnText' => str_random(100),
+                'linkOnAudio' => str_random(100),
+                'linkOnVideo' => str_random(100),
+                'linkOnDemoText' => str_random(100),
+                'linkOnDemoAudio' => str_random(100),
+                'linkOnDemoVideo' => str_random(100),
+                'type' => random_int(1,3),
+                'amountOfDownloadsDemo' => 0,
+            ]);
+        }
+    }
+}
